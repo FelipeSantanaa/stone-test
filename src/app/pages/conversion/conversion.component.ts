83fe4491642be0c5ff9ab1361b05444312398c6c
Felipe Alves <felipe008_@hotmail.com>
2024-07-03 17:47:28 -0300

    feat/ homepage

--- src/app/pages/conversion/conversion.component.ts
+++ src/app/pages/conversion/conversion.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { Router } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-conversion',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [CommonModule, HeaderComponent, ReactiveFormsModule],
   templateUrl: './conversion.component.html',
-  styleUrl: './conversion.component.css'
+  styleUrl: './conversion.component.css',
 })
 export class ConversionComponent {
+  form!: FormGroup;
+  valor!: number;
+  taxaDoEstado!: number;
+  areInputsValid: boolean = true;
 
+  constructor(private router: Router) {
+    this.form = new FormGroup({
+      valor: new FormControl('', [Validators.required, Validators.min(0)]),
+      taxaDoEstado: new FormControl('', [
+        Validators.required,
+        Validators.min(0),
+      ]),
+      tipoDeCompra: new FormControl('', [Validators.required]),
+    });
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    this.router.navigate(['/result']);
+  }
 }
