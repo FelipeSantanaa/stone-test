b8b49ef98ae876ccda5d5ae56cc85532dfc84926
Felipe Alves <felipe008_@hotmail.com>
2024-07-03 14:26:45 -0300

    feat/ add angular routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ConversionComponent } from './pages/conversion/conversion.component';
+import { ResultComponent } from './pages/result/result.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: ConversionComponent
+    },
+    {
+        path: 'result', component: ResultComponent
+    }
+];
