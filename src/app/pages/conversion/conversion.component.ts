9773a52e65c4d8db59cd536028764b96a679b761
Felipe Alves <felipe008_@hotmail.com>
2024-07-03 23:57:41 -0300

    feat/ add result page

--- src/app/pages/conversion/conversion.component.ts
+++ src/app/pages/conversion/conversion.component.ts
@@ -13,20 +13,23 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { MoedaService } from '../../service/moeda.service';
 
 import { moedaInterface } from '../../interface/moeda-interface';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
 
 @Component({
   selector: 'app-conversion',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, ReactiveFormsModule],
+  imports: [CommonModule, HeaderComponent, ReactiveFormsModule, CurrencyMaskModule],
   templateUrl: './conversion.component.html',
   styleUrl: './conversion.component.css',
 })
 export class ConversionComponent {
   form!: FormGroup;
-  valor!: number;
-  taxaDoEstado!: number;
-  areInputsValid: boolean = true;
+  valor: number = 0;
+  taxaDoEstado: number = 8.87;
+  dolar!: any;
   moedaReturn?: moedaInterface;
+  iofMoney: number = 1.1;
+  iofCard: number = 6.4;
 
   constructor(private router: Router, private moedaService: MoedaService) {
     this.form = new FormGroup({
@@ -43,7 +46,26 @@ export class ConversionComponent {
     this.getDolar();
   }
 
+  calculateValor(){
+    if (this.form.valid) {
+      const valor = this.form.get('valor')?.value;
+      const taxaDoEstado = this.form.get('taxaDoEstado')?.value;
+      const tipoDeCompra = this.form.get('tipoDeCompra')?.value;
+      this.dolar = this.moedaReturn?.bid;
+      console.log(valor, taxaDoEstado, tipoDeCompra, this.dolar);
+      if (tipoDeCompra === 'dinheiro') {
+        this.valor = valor * this.dolar;
+      } else {
+        this.valor = valor * this.dolar;
+        this.valor = this.valor + this.valor * (taxaDoEstado / 100);
+      }
+    } else {
+    }
+  
+  }
+
   onSubmit() {
+    this.calculateValor();
     this.router.navigate(['/result']);
   }
 
--- src/app/pages/result/result.component.ts
+++ src/app/pages/result/result.component.ts
@@ -1,9 +1,11 @@
 import { Component, Output } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-result',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './result.component.html',
   styleUrl: './result.component.css'
 })
@@ -11,5 +13,19 @@ export class ResultComponent {
   @Output() valorCalculo!: number;
   @Output() taxa!: number;
   @Output() currentDolar!: number;
+
+  constructor(
+    private route: Router
+  ){
+
+  }
+
+  ngOnInit(){
+
+  }
+
+  backToHome(){
+    this.route.navigate(['']);
+  }
 
 }
